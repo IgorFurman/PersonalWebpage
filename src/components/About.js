c6ddb89f90c646efbd0d5b0f41bf4735056b9338
Igor <igorfurman555@gmail.com>
2023-04-24 14:52:35 +0200

    added banner section and started aboutus section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+// countup
+import CountUp from 'react-countup'
+// observer hook
+import {useInView} from 'react-intersection-observer'
+// motion
+import {motion} from 'framer-motion'
+// variant 
+import {fadeIn} from '../variants'
 
 const About = () => {
-  return <div 
+  return <section 
   className ='section'
-  id='about'>About</div>;
+  id='about'>About</section>;
 };
 
 export default About;
