7e9d2c8984847671065ed3d71bb71c71dbb43c2d
IgorFurman <123864445+IgorFurman@users.noreply.github.com>
2023-06-26 13:36:05 +0200

    skills component added

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,58 +1,101 @@
+
 import React from 'react';
-import { Carousel } from 'react-carousel3';
 import { motion } from 'framer-motion';
-import { fadeIn } from '../variants';
+import { fadeIn } from '../variants'
+import { useInView } from 'react-intersection-observer';
 
-import JsLogo from '../assets/javascript-1.svg';
-import Html5Logo from '../assets/html5-logo.svg';
-import ReactLogo from '../assets/react-logo.svg';
-import ReduxLogo from '../assets/redux-logo.svg';
-import CssLogo from '../assets/css-logo.svg';
-import SassLogo from '../assets/sass-logo.svg';
+const actualSkills = [
+  "Semantic & Acessible HTML",
+  "Responsible Web Design",
+  "Teamwork",
+  "Markdown",
+  "Immutability",
+  "CSS BEM convention",
+  "CSS Grid",
+  "CSS Flexbox",
+  "Tailwind",
+  "Bootstrap",
+  "Styled Components",
+  "React Router",
+  "React Context",
+  "Redux (Toolkit)",
+  "Redux-Saga",
+  "React Hooks",
+  "Error handling",
+  "API (fetch, axios)",
+  "JavaScript (ES6+)",
+  "Promises, Async/Await",
+  "GitHub Pull Requestest & Review",
+  "Trello",
+  "Scrum",
+  "NPM",
+  "React",
+  "Git",
+  "Node.js (basics)",
+  "Express (basics)",
+];
 
-const technologies = [
-  { name: "JavaScript", logo: JsLogo },
-  { name: "HTML5", logo: Html5Logo },
-  { name: "React", logo: ReactLogo },
-  { name: "Redux", logo: ReduxLogo },
-  { name: "CSS", logo: CssLogo },
-  { name: "SASS", logo: SassLogo }
-]
+const futureSkills = [
+  "TypeScript",
+  "JS Classes",
+  "Unit testing",
+  "Cypress",
+  "React Native",
+];
+
 
 const Skills = () => {
-	return (
-		<section className='min-h-screen md:mt-[100px] lg:mt-[300px] lg:mb-0' id='skills'>
-			<div className='container mx-auto '>
-				<motion.h2
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section id="skills" className="container mx-auto p-4 md:p-8" ref={ref}>
+      	<motion.h2
 					variants={fadeIn('up', 0.3)}
 					initial='hidden'
 					whileInView={'show'}
 					viewport={{ once: false, amount: 0.7 }}
-					className='h2 p-5 md:mb-[200px] text-gradient text-lg border solid '
+					className='h2 p-5 text-accent text-center'
 				>
-					Technology stack.
+					Skills
 				</motion.h2>
-        <div className="md:flex hidden content-center ">
-				  <Carousel height={500 + 'px'} width={100 + '%'} autoPlay={true} >
-					  {technologies.map((tech, index) => (
-              <div key={index} className='flex justify-items-center '>
-                <img alt={`${tech.name} logo`} src={tech.logo} />
-								<p className='mt-2 text-center'>{tech.name}</p>
-              </div>
+      <div className="space-y-4 md:space-y-8">
+        <motion.div className="mb-4 p-4 border border-gray-300 rounded-md shadow-md"
+          variants={fadeIn('right', 0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.3 }}>
+          <h3 className="text-2xl font-semibold mb-2">My skillset:</h3>
+          <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-4">
+            {actualSkills.map((skill, index) => (
+              <li key={index} className="list-disc ml-5">
+                {skill}
+              </li>
             ))}
-				  </Carousel>
-        </div>
-        <div className="md:hidden">
-          {technologies.map((tech, index) => (
-            <div key={index} className="flex flex-col items-center">
-              <img className='mt-5' alt={`${tech.name} logo`} src={tech.logo} />
-              <p className='font-primary'>{tech.name}</p>
-            </div>
-          ))}
-        </div>
-			</div>
-		</section>
-	);
+          </ul>
+        </motion.div>
+        <motion.div className="p-4 border border-gray-300 rounded-md shadow-md"
+          variants={fadeIn('left', 0.3)}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.3 }}>
+          <h3 className="text-2xl font-semibold mb-2">My future skillset:</h3>
+          <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-4">
+            {futureSkills.map((futureSkill, index) => (
+              <li key={index} className="list-disc ml-5">
+                {futureSkill}
+              </li>
+            ))}
+          </ul>
+        </motion.div> 
+      </div>
+    </ section>
+  );
 };
 
 export default Skills;
+
+
+
+
+
